dd47307ed0ea9db475a88be5623f770fdec07963
HernanGomezM <dariog_36@hotmail.com>
2024-12-04 16:35:14 -0500

    inciamos hacemos movible el boton para que el menu se ajuste con este cosa que se va a cambiar

--- Componentes/App.js
+++ Componentes/App.js
@@ -5,27 +5,46 @@ import Draggable from "react-native-draggable";
 import Menu from "./Menu";
 const App = () => {
   const [menuVisible, setMenuVisible] = useState(false);
-
+  const [pocionBotonDrag, setPocionBotonDrag] = useState({ x: 0, y: 0 });
   useEffect(() => {
     setMenuVisible(false);
+    setPocionBotonDrag({ x: 343, y: 807 });
+    console.log("Pocicionamiento incial", pocionBotonDrag);
   }, []);
 
-  const handlePress = () => {
-    setMenuVisible(true);
+  const handlePressT = () => {
+    if (menuVisible) {
+      setMenuVisible(false);
+      console.log("tocando el btn", menuVisible);
+    } else {
+      setMenuVisible(true);
+      console.log("tocando el btn", menuVisible);
+    }
   };
+  const handleDragRelease = (event, gestureState, bounds) => {
+    const { moveX, moveY } = gestureState;
+    setPocionBotonDrag({ x: moveX, y: moveY });
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      {menuVisible ? <Menu /> : <Text>Error</Text>}
+
       <Draggable
         x={310}
         y={750}
         renderSize={56}
         renderColor="#F1AC20"
         isCircle
-        shouldReverse
-        onShortPressRelease={handlePress}
+        bounds="parent"
+        onPressOut={handlePressT}
+        onDragRelease={handleDragRelease}
       />
+      {menuVisible ? (
+        <Menu pocionBotonDrag={pocionBotonDrag} />
+      ) : (
+        <Text>Touche the bottom to see the menu</Text>
+      )}
     </View>
   );
 };
--- Componentes/Menu.js
+++ Componentes/Menu.js
@@ -1,14 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
 import Draggable from "react-native-draggable";
 
-const Menu = () => {
+const Menu = ({ pocionBotonDrag }) => {
+  console.log(
+    "Pocioconamiento con las props",
+    " X:",
+    Math.trunc(pocionBotonDrag.x),
+    " y:",
+    Math.trunc(pocionBotonDrag.y)
+  );
   return (
-    <View style={style.Menu}>
-      <Text style={style.MenuText}>Hola mundo</Text>
-    </View>
+    <Draggable
+      x={Math.trunc(pocionBotonDrag.x)}
+      y={Math.trunc(pocionBotonDrag.y)}
+      bounds="parent"
+      children={
+        <View style={style.Menu}>
+          <Text style={style.MenuText}>Hola mundo</Text>
+        </View>
+      }
+    />
   );
 };
-
 const style = StyleSheet.create({
   Menu: {
     borderRadius: 10,
@@ -18,12 +31,13 @@ const style = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     position: "absolute",
-    bottom: 80,
-    right: 95,
+    bottom: 80, //80
+    right: 95, //95
   },
   MenuText: {
     color: "#fff",
     fontSize: 14,
   },
 });
+
 export default Menu;
