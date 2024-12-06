8ee8ae460b448ff8296d0712f46a8afa1831dfe4
HernanGomezM <dariog_36@hotmail.com>
2024-12-06 18:46:57 -0500

    Se realiza el primer formarto de nota pero tiene un problema de vista tambien se crean 3 botones para la nota

--- Componentes/NuevaNota.js
+++ Componentes/NuevaNota.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Pressable } from "react-native";
-import { Modal, Button, Portal, TextInput } from "react-native-paper";
+import {
+  StyleSheet,
+  TextInput,
+  Text,
+  Pressable,
+  SafeAreaView,
+} from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Modal, Portal } from "react-native-paper";
 //---------------------------------------------
 const NotaNueva = ({ onClick }) => {
   //---------------------------------------------
@@ -22,24 +29,41 @@ const NotaNueva = ({ onClick }) => {
         onDismiss={hideModal}
         contentContainerStyle={styles.notaNuevaContainer}
       >
-        <TextInput
-          label="Título de la Nota"
-          value={tituloTexto}
-          mode="outlined"
-          style={styles.txtTitulo}
-          onChangeText={setTituloTexto}
-          multiline={false}
-        />
-        <TextInput
-          label="Contenido de la Nota"
-          value={notaTexto}
-          mode="outlined"
-          style={styles.txtNota}
-          onChangeText={setNotaTexto}
-          multiline={true}
-          textAlignVertical="top"
-        />
-        <Pressable style={styles.btnClose} onPress={hideModal}></Pressable>
+        <SafeAreaView style={styles.SafeAreaViewContainer}>
+          <TextInput
+            editable
+            multiline
+            numberOfLines={1}
+            placeholder="Titulo:"
+            placeholderTextColor="#6B6B6B"
+            onChangeText={setTituloTexto}
+            value={tituloTexto}
+            color="#FFF"
+            style={styles.txtTitulo}
+          />
+          <TextInput
+            editable
+            multiline={true}
+            placeholder="Nota:"
+            placeholderTextColor="#6B6B6B"
+            onChangeText={setNotaTexto}
+            value={notaTexto}
+            color="#FFF"
+            style={styles.txtNota}
+          />
+        </SafeAreaView>
+        <Pressable style={styles.btnConfiguracion} onPress={hideModal}>
+          <Text style={{ fontSize: 18 }}>⚙️</Text>
+        </Pressable>
+        <Pressable style={styles.btnClose} onPress={hideModal}>
+          <Text style={{ fontSize: 18, color: "black" }}>X</Text>
+        </Pressable>
+        <Pressable style={styles.btnGuardar} onPress={hideModal}>
+          <Text style={{ fontSize: 18 }}>💾</Text>
+        </Pressable>
+        {/* <Pressable style={styles.btnEditar} onPress={hideModal}>
+          <Text style={{ fontSize: 18 }}>📝</Text>
+        </Pressable> */}
       </Modal>
     </Portal>
   );
@@ -53,13 +77,26 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     backgroundColor: "white",
     borderRadius: 15,
-    justifyContent: "flex-start",
+    justifyContent: "",
     alignItems: "center",
+  },
+  SafeAreaViewContainer: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    padding: 0,
+    marginLeft: 0,
+    backgroundColor: "white",
+    borderRadius: 15,
+    justifyContent: "flex-start",
+    alignItems: "end",
   },
   txtTitulo: {
     fontSize: 18,
     width: "100%",
     marginBottom: 10,
+    backgroundColor: "#6A9FA6",
+    borderRadius: 10,
   },
   txtNota: {
     fontSize: 16,
@@ -67,17 +104,53 @@ const styles = StyleSheet.create({
     height: 590,
     marginBottom: 10,
     textAlignVertical: "top",
+    backgroundColor: "#6A9FA6",
+    borderRadius: 10,
   },
   btnClose: {
     position: "absolute",
+    backgroundColor: "#F1AC20",
     bottom: 20,
     right: 20,
-    width: 50,
-    height: 50,
-    borderRadius: 25,
+    width: 40,
+    height: 40,
+    borderRadius: 15,
     justifyContent: "center",
     alignItems: "center",
   },
+  btnConfiguracion: {
+    position: "absolute",
+    backgroundColor: "#F1AC20",
+    bottom: 20,
+    right: 63,
+    width: 40,
+    height: 40,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnGuardar: {
+    position: "absolute",
+    backgroundColor: "#F1AC20",
+    bottom: 20,
+    right: 106,
+    width: 40,
+    height: 40,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  // btnEditar: {
+  //   position: "absolute",
+  //   backgroundColor: "#F1AC20",
+  //   bottom: 20,
+  //   right: 149,
+  //   width: 40,
+  //   height: 40,
+  //   borderRadius: 15,
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  // },
 });
 
 export default NotaNueva;
