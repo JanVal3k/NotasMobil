6e421617b2cf7c30adfb5cffb279664dec3464a2
HernanGomezM <dariog_36@hotmail.com>
2024-12-18 17:12:26 -0500

    Se crea modal para escribir la tarea y asi dejar el resto del componente para ver las tareas creadas

--- Componentes/Calendario.js
+++ Componentes/Calendario.js
@@ -6,6 +6,9 @@ import {
   View,
   StatusBar,
   ScrollView,
+  Modal,
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
 import { DatePickerModal, registerTranslation } from 'react-native-paper-dates';
 import GuardarYMostrarNotas from './Clases/GuardarYMostrarNotas';
@@ -15,6 +18,7 @@ registerTranslation('es', es);
 const Calendario = () => {
   const { estadoGlobal, setEstadoGlobal } = useEstadoGlobal();
   const [tareas, setTareas] = useState([]);
+  const [nuevaTarea, setNuevaTarea] = useState(false);
   const [fecha, setFecha] = useState(new Date());
   const [calenVisible, setCalenVisible] = useState(false);
 
@@ -59,11 +63,8 @@ const Calendario = () => {
   return (
     <View style={styles.viewContainer}>
       <StatusBar style="light" />
-      <ScrollView>
-        <Pressable style={styles.presseable} onPress={() => abrirCalendario()}>
-          <Text style={styles.txtPresseable}>📅</Text>
-        </Pressable>
-      </ScrollView>
+      <ScrollView></ScrollView>
+
       <DatePickerModal
         animationType="slide"
         locale="es"
@@ -73,6 +74,33 @@ const Calendario = () => {
         onConfirm={() => onConfirm()}
         date={fecha}
       />
+      <TouchableOpacity
+        style={styles.extraButton}
+        onPress={() => setNuevaTarea(true)}
+      >
+        <Text style={styles.extraButtonText}>+</Text>
+      </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={nuevaTarea}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setNuevaTarea(!nuevaTarea);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => abrirCalendario()}
+            >
+              <Text style={styles.textStyle}>📅</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -93,6 +121,56 @@ const styles = StyleSheet.create({
   },
   txtPresseable: {
     fontSize: 30,
+  },
+  extraButton: {
+    borderRadius: 12,
+    top: -50,
+    right: -150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 60,
+    height: 60,
+    backgroundColor: '#5C6570',
+  },
+  extraButtonText: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 export default Calendario;
